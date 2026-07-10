b0a99d26cdd490fdf7b7fbb65a55debd0f4033a9
Michael Wong <40144792+glowmachine@users.noreply.github.com>
2026-07-10 13:27:26 +1000

    chore: add state abbr to names map

---
+++ src/types/states.ts
@@ -0,0 +1,97 @@
+export const states: Record<string, string> = {
+    // Other
+    DC: 'District of Columbia',
+    AS: 'American Samoa',
+    GU: 'Guam',
+    MP: 'Northern Mariana Islands',
+    PR: 'Puerto Rico',
+    VI: 'Virgin Islands',
+
+    // A
+    AK: 'Alaska',
+    AL: 'Alabama',
+    AR: 'Arkansas',
+    AZ: 'Arizona',
+
+    // C
+    CA: 'California',
+    CT: 'Connecticut',
+    CO: 'Colorado',
+
+    // D
+    DE: 'Delaware',
+
+    // F
+    FL: 'Florida',
+
+    // G
+    GA: 'Georgia',
+
+    // H
+    HI: 'Hawaii',
+
+    // I
+    ID: 'Idaho',
+    IL: 'Illinois',
+    IA: 'Iowa',
+    IN: 'Indiana',
+
+    // K
+    KS: 'Kansas',
+    KY: 'Kentucky',
+
+    // L
+    LA: 'Louisiana',
+
+    // M
+    MS: 'Mississippi',
+    MT: 'Montana',
+    MA: 'Massachusetts',
+    MD: 'Maryland',
+    ME: 'Maine',
+    MI: 'Michigan',
+    MN: 'Minnesota',
+    MO: 'Missouri',
+
+    // N
+    NC: 'North Carolina',
+    ND: 'North Dakota',
+    NE: 'Nebraska',
+    NH: 'New Hampshire',
+    NJ: 'New Jersey',
+    NM: 'New Mexico',
+    NV: 'Nevada',
+    NY: 'New York',
+
+    // O
+    OH: 'Ohio',
+    OK: 'Oklahoma',
+    OR: 'Oregon',
+
+    // P
+    PA: 'Pennsylvania',
+
+    // R
+    RI: 'Rhode Island',
+
+    // S
+    SD: 'South Dakota',
+    SC: 'South Carolina',
+
+    // T
+    TN: 'Tennessee',
+    TX: 'Texas',
+
+    // U
+    UT: 'Utah',
+
+    // V
+    VT: 'Vermont',
+    VA: 'Virginia',
+
+    // W
+    WA: 'Washington',
+    WI: 'Wisconsin',
+    WV: 'West Virginia',
+    WY: 'Wyoming',
+};
