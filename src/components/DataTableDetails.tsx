40ad1e5993527b798fa71a654ccff33fa8e4c49c
Michael Wong <40144792+glowmachine@users.noreply.github.com>
2026-07-15 16:02:56 +1000

    chore: rm unused component

--- src/components/DataTableDetails.tsx
+++
@@ -1,8 +0,0 @@
-export default function DataTableDetails() {
-    return (
-        <div>
-            <h1>Jane Doe</h1>
-            <h2>Representative</h2><span>Massachusetts, 3rd District</span>
-        </div>
-    );
-}
