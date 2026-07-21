6b68f6decc7cb1dd5487c39a5a56eba592ea2741
Michael Wong <40144792+glowmachine@users.noreply.github.com>
2026-07-21 11:54:08 +1000

    feat: add yaml ouput to Details pages

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,75 +1,81 @@
 import type { DataType } from "../api/fetchData";
 import { useDataContext } from "../contexts/DataContext";
-import type { Bio, ID, LeadershipRole, Name, Term } from "../types/legislatorsCurrent";
-import { states } from "../types/states";
+import type { LeadershipRole } from "../types/legislatorsCurrent";
+import { allAreas, type StateAbbreviation } from "../types/states";
 import getDateDiff from "../util/getDateDiff";
 import { NavLink } from "react-router";
+import { renderLegislatorsCurrent } from "./renderLegislatorsCurrent";
 
-const idKeys: Array<keyof ID> = [
-    'bioguide',
-    'thomas',
-    'lis',
-    'govtrack',
-    'opensecrets',
-    'votesmart',
-    'fec',
-    'cspan',
-    'wikipedia',
-    'house_history',
-    'ballotpedia',
-    'maplight',
-    'icpsr',
-    'wikidata',
-    'google_entity_id',
-    'pictorial'
-];
-const nameKeys: Array<keyof Name> = [
-    'first',
-    'middle',
-    'last',
-    'suffix',
-    'official_full',
-    'nickname',
-];
-const bioKeys: Array<keyof Bio> = [
-    'birthday',
-    'gender',
-];
-const termKeys: Array<keyof Term> = [
-    'party',
-    'type',
-    'class',
-    'state',
-    'district',
-    'start',
-    'end',
-    'url',
-    'address',
-    'phone',
-    'fax',
-    'contact_form',
-    'office',
-    'state_rank',
-    'rss_url',
-    'caucus',
-    'how',
-    'end-type',
-    'party_affiliations',
-];
-const leadershipKeys: Array<keyof LeadershipRole> = [
-    'title',
-    'chamber',
-    'start',
-    'end',
-];
+// const idKeys: Array<keyof ID> = [
+//     'bioguide',
+//     'thomas',
+//     'lis',
+//     'govtrack',
+//     'opensecrets',
+//     'votesmart',
+//     'fec',
+//     'cspan',
+//     'wikipedia',
+//     'house_history',
+//     'ballotpedia',
+//     'maplight',
+//     'icpsr',
+//     'wikidata',
+//     'google_entity_id',
+//     'pictorial'
+// ];
+// const nameKeys: Array<keyof Name> = [
+//     'first',
+//     'middle',
+//     'last',
+//     'suffix',
+//     'official_full',
+//     'nickname',
+// ];
+// const bioKeys: Array<keyof Bio> = [
+//     'birthday',
+//     'gender',
+// ];
+// const termKeys: Array<keyof Term> = [
+//     'party',
+//     'type',
+//     'class',
+//     'state',
+//     'district',
+//     'start',
+//     'end',
+//     'url',
+//     'address',
+//     'phone',
+//     'fax',
+//     'contact_form',
+//     'office',
+//     'state_rank',
+//     'rss_url',
+//     'caucus',
+//     'how',
+//     'end-type',
+//     'party_affiliations',
+// ];
+// const leadershipKeys: Array<keyof LeadershipRole> = [
+//     'title',
+//     'chamber',
+//     'start',
+//     'end',
+// ];
 
-function getPartyAbbr(party: string | undefined) {
+function abbreviateParty(party: string | undefined) {
     switch (party) {
         case 'Democrat': return 'D';
         case 'Independent': return 'I';
         case 'Republican': return 'R';
         default: return party;
     }
+}
+
+function getLeadershipRole(terms: LeadershipRole[] | undefined) {
+    if (!terms || terms[terms.length - 1].end) return;
+    return <p>{terms[terms.length - 1].title}</p>
 }
 
 interface DetailsProps {
@@ -86,53 +92,28 @@ export default function Details({ bioguide }: DetailsProps) {
     return (
         <article>
             <NavLink to='/' className='rounded bg-gray-300 active:bg-gray-400'>🔙</NavLink>
-            <div className='border-b p-1 w-full flex gap-2'>
+            <section className='border-b p-1 w-full flex gap-2'>
                 <img alt={`Profile photo for ${info.name.first} ${info.name.last}`}
                     src={`https://unitedstates.github.io/images/congress/225x275/${info.id.bioguide}.jpg`}
                     className='w-30'
                 />
                 <div>
-                    <p>{`${info.name.first} ${info.name.last}, ${age}${info.bio.gender}`}</p>
-                    <p>{`(${getPartyAbbr(currentTerm.party)})${currentTerm.type === 'rep' ? 'Representative' : 'Senator'}`}</p>
-                    <p>{`${states[currentTerm.state]}${currentTerm.district ? ', District ' + currentTerm.district : ''}`}</p>
+                    <div className='mb-1'>
+                        <p>{info.name.first} {info.name.last}</p>
+                        <p>{age}{info.bio.gender}</p>
+                    </div>
+                    <div className='mb-1'>
+                        <p>{`(${abbreviateParty(currentTerm.party)})${currentTerm.type === 'rep' ? 'Representative' : 'Senator'}`}</p>
+                        <p>{`${allAreas[currentTerm.state as StateAbbreviation]}${currentTerm.district ? ', District ' + currentTerm.district : ''}`}</p>
+                    </div>
+                    {getLeadershipRole(info.leadership_roles)}
                 </div>
-            </div>
-            <div className='bg-gray-200 flex flex-col gap-5'>
-                <div>
-                    {nameKeys.map(k => <p key={k}>{k}: {info.name[k]}</p>)}
-                </div>
-                <div>
-                    {bioKeys.map(k => <p key={k}>{k}: {info.bio[k]}</p>)}
-                </div>
-                <div>
-                    {idKeys.map(k => <p key={k}>{k}: {info.id[k]}</p>)}
-                </div>
-                <div>
-                    <p>Current Term:</p>
-                    {termKeys.map(k => <p key={k}>{k}: {`${currentTerm[k] ? currentTerm[k] : ''}`}</p>)}
-                </div>
-                <div>
-                    <p>All Terms:</p>
-                    {info.terms.map(term =>
-                        <details key={term.start}>
-                            <summary className='hover:cursor-pointer'>{term.start} - {term.end}</summary>
-                            {termKeys.map(k => <p key={k}>{k}: {`${term[k] ? term[k] : ''}`}</p>)}
-                        </details>
-                    )
-                    }
-                </div>
-                <div>
-                    <p>Leadership Roles:</p>
-                    {info.leadership_roles
-                        ? info.leadership_roles.map(term =>
-                            <details key={term.start}>
-                                <summary className='hover:cursor-pointer'>{term.start} - {term.end}</summary>
-                                {leadershipKeys.map(k => <p key={k}>{k}: {`${term[k] ? term[k] : ''}`}</p>)}
-                            </details>
-                        )
-                        : <p>none</p>}
-                </div>
-            </div>
-        </article >
+            </section>
+            <section>
+            </section>
+            <details open><summary className='font-bold hover:cursor-pointer'>legislatorsCurrent.yaml</summary>
+                {renderLegislatorsCurrent(info)}
+            </details>
+        </article>
     );
 }
---
+++ src/components/renderLegislatorsCurrent.tsx
@@ -0,0 +1,164 @@
+import type { DataType } from "../api/fetchData";
+import type { Bio, ID, LeadershipRole, Name, PartyAffiliation, Term } from "../types/legislatorsCurrent";
+
+const idKeys: Array<keyof ID> = [
+    'bioguide',
+    'thomas',
+    'lis',
+    'govtrack',
+    'opensecrets',
+    'votesmart',
+    'fec',
+    'cspan',
+    'wikipedia',
+    'house_history',
+    'ballotpedia',
+    'maplight',
+    'icpsr',
+    'wikidata',
+    'google_entity_id',
+    'pictorial'
+];
+const nameKeys: Array<keyof Name> = [
+    'first',
+    'middle',
+    'last',
+    'suffix',
+    'nickname',
+    'official_full',
+];
+const bioKeys: Array<keyof Bio> = [
+    'birthday',
+    'gender',
+];
+const termKeys: Array<keyof Term> = [
+    'type',
+    'start',
+    'end',
+    'state',
+    'district',
+    'class',
+    'state_rank',
+    'party',
+    'party_affiliations',
+    'url',
+    'address',
+    'phone',
+    'fax',
+    'contact_form',
+    'office',
+    'rss_url',
+    'caucus',
+    'how',
+    'end-type',
+];
+const affiliationKeys: Array<keyof PartyAffiliation> = [
+    'start',
+    'end',
+    'party',
+    'caucus',
+]
+const leadershipKeys: Array<keyof LeadershipRole> = [
+    'title',
+    'chamber',
+    'start',
+    'end',
+];
+
+function indent(count: number): string {
+    if (count <= 0) return '';
+    return Array(count).fill('\u00A0').join('');
+}
+
+function isDateKey(str: string): boolean {
+    return (str === 'birthday' || str === 'start' || str === 'end');
+}
+
+function styleMissing(obj: unknown) {
+    return (obj === undefined)
+        ? 'text-gray-400'
+        : ''
+}
+
+//YAML formatting reconstructed from json data, an absolute mess, needs refactoring
+export function renderLegislatorsCurrent(member: DataType) {
+    return (
+        <section className='font-mono'>
+            <ul>
+                <li>- id:</li>
+                {idKeys.slice(1).map(key => key !== 'fec'
+                    ? <li className={styleMissing(member.id[key])} key={key}>{indent(4)}{key}: {member.id[key]}</li>
+                    : <li key={key}><ul>
+                        <li>{indent(4)}fec:</li>
+                        {member.id[key]?.map(item => <li key={key}>
+                            {indent(4)}- {item}
+                        </li>)}
+                    </ul></li>)}
+                <ul>
+                    <li>{indent(2)}name:</li>
+                    {nameKeys.map(key => <li className={styleMissing(member.name[key])} key={key}>
+                        {indent(4)}{key}: {member.name[key]}
+                    </li>)}
+                </ul>
+                <ul>
+                    <li>{indent(2)}bio:</li>
+                    {bioKeys.map(key => isDateKey(key)
+                        ? <li key={key}>{indent(4)}{key}: '{member.bio[key]}'</li>
+                        : <li key={key}>{indent(4)}{key}: {member.bio[key]}</li>)}
+                </ul>
+
+                {/* <ul>
+                    <li>{indent(2)}leadership_roles:</li>
+                    <li>
+                        {member.terms.map((term, index) => <ul key={index}>
+                            <li>{indent(2)}- type: {term.type}</li>
+                            {termKeys.slice(1).map(key => key !== 'party_affiliations'
+                                ? isDateKey(key)
+                                    ? <li className={styleMissing(term[key])} key={key}>{indent(4)}{key}: '{term[key]}'</li>
+                                    : <li className={styleMissing(term[key])} key={key}>{indent(4)}{key}: {term[key]}</li>
+                                : term['party_affiliations'] && <li key={key}><ul>
+                                    <li>{indent(4)}party_affiliations:</li>
+                                    {term[key]?.map(item =>
+                                        <ul>
+                                            <li>{indent(4)}- start: '{item['start']}'</li>
+                                            {affiliationKeys.slice(1).map(key => isDateKey(key)
+                                                ? <li className={styleMissing(item[key])} key={key}>{indent(6)}{key}: '{item[key]}'</li>
+                                                : <li className={styleMissing(item[key])} key={key}>{indent(6)}{key}: {item[key]}</li>
+                                            )}
+                                        </ul>
+                                    )}
+                                </ul></li>
+                            )}
+                        </ul>)}
+                    </li>
+                </ul> */}
+
+                <ul>
+                    <li>{indent(2)}terms:</li>
+                    <li>
+                        {member.terms.map((term, index) => <ul key={index}>
+                            <li>{indent(2)}- type: {term.type}</li>
+                            {termKeys.slice(1).map(key => key !== 'party_affiliations'
+                                ? isDateKey(key)
+                                    ? <li className={styleMissing(term[key])} key={key}>{indent(4)}{key}: '{term[key]}'</li>
+                                    : <li className={styleMissing(term[key])} key={key}>{indent(4)}{key}: {term[key]}</li>
+                                : term['party_affiliations'] && <li key={key}><ul>
+                                    <li>{indent(4)}party_affiliations:</li>
+                                    {term[key]?.map(item =>
+                                        <ul>
+                                            <li>{indent(4)}- start: '{item['start']}'</li>
+                                            {affiliationKeys.slice(1).map(key => isDateKey(key)
+                                                ? <li className={styleMissing(item[key])} key={key}>{indent(6)}{key}: '{item[key]}'</li>
+                                                : <li className={styleMissing(item[key])} key={key}>{indent(6)}{key}: {item[key]}</li>
+                                            )}
+                                        </ul>
+                                    )}
+                                </ul></li>
+                            )}
+                        </ul>)}
+                    </li>
+                </ul>
+            </ul>
+        </section>
+    )
+}
