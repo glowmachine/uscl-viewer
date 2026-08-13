49eb88348275fc63301187298b508ad46cfbff7f
Michael Wong <40144792+glowmachine@users.noreply.github.com>
2026-08-13 16:47:24 +1000

    bug: include target _blank to link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 export default function Footer() {
     return <footer className='bg-red-200 flex justify-center'>
         <a href='https://github.com/glowmachine'
+            target='_blank'
             className='hover:underline'
         >Github</a>
     </footer>;
