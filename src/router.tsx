4f202f0b7afe8d289541a24f3b3096b8dd53e131
Michael Wong <40144792+glowmachine@users.noreply.github.com>
2026-08-14 14:50:16 +1000

    refactor: rename directory DataTable to List

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,16 @@
 import { createBrowserRouter } from "react-router";
 import App from "./App";
 import DetailsRoute from "./pages/DetailsRoute";
-import Table from "./components/DataTable/Table";
+import Table from "./components/List/Table";
 import NotFound from "./components/NotFound";
+import Dashboard from "./components/Dashboard";
 
 export const router = createBrowserRouter([
     {
         path: '/', element: <App />,
         children: [
             { index: true, element: <Table /> },
+            { path: '/dashboard', element: <Dashboard /> },
             { path: '/table', element: <Table /> },
             { path: '/details/:bioguide', element: <DetailsRoute /> },
             { path: '*', element: <NotFound /> },
