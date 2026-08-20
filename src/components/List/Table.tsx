6845a32d19de7b5077da92a58928c965073fc8f4
Michael Wong <40144792+glowmachine@users.noreply.github.com>
2026-08-20 21:33:18 +1000

    refactor: move FilterSelector to Search

--- src/components/List/Table.tsx
+++ src/components/List/Table.tsx
@@ -6,13 +6,13 @@ import getRowData from "./getRowData";
 import sortRows from "./sortRows";
 import filterData from "./filterData";
 import ColumnSelector from "./ColumnSelector";
-import FilterSelector from "./FilterSelector";
+
+const buttonStyle = 'h-10 w-10 flex items-center justify-center rounded-full hover:bg-gray-200';
 
 export default function Table() {
     const { legislators, isLoading, error } = useDataContext();
     const { columns, sortBy, setSortBy, filterOptions } = useTableContext();
     const [colSelectOpen, setColSelectOpen] = useState(false);
-    const [filterSelectOpen, setFilterSelectOpen] = useState(false);
 
     const rows = useMemo<Row[]>(() => {
         if (!legislators) return [];
@@ -39,32 +39,7 @@ export default function Table() {
     return (<>
         {colSelectOpen && <ColumnSelector colSelectOpen={colSelectOpen} setColSelectOpen={setColSelectOpen} />}
 
-        <div className='flex flex-col min-w-0 max-h-full'>
-            <div className='m-5 flex items-center gap-1 justify-between'>
-                <div className='flex items-center gap-1'>
-                    <h1 className='text-3xl'>Legislators</h1> <span>({rows.length})</span>
-                </div>
-                <FilterSelector open={filterSelectOpen} />
-                <div className='flex gap-1'>
-                    <button className={'h-10 w-10 flex items-center justify-center rounded-full bg-gray-200'}
-                        onClick={() => setFilterSelectOpen(prev => !prev)}>
-                        <span className='material-symbols-outlined'
-                            style={{ fontVariationSettings: `'FILL' ${filterSelectOpen ? 1 : 0}` }}
-                        >
-                            filter_alt
-                        </span>
-                    </button>
-                    <button className='h-10 w-10 flex items-center justify-center rounded-full bg-gray-200'
-                        onClick={() => setColSelectOpen(prev => !prev)}
-                    >
-                        <span className='material-symbols-outlined'
-                            style={{ fontVariationSettings: `'FILL' ${colSelectOpen ? 1 : 0}` }}
-                        >
-                            view_column
-                        </span>
-                    </button>
-                </div>
-            </div>
+        <div className='bg-gray-300 p-4 flex flex-col min-w-0 max-h-full h-screen'>
             {/* <div id='page-controls' className='self-end m-1 flex items-center gap-1'>
                 {rows.length === 0
                     ? <span>0-0 of 0</span>
@@ -90,34 +65,53 @@ export default function Table() {
                     text-3xl text-gray-400'>Loading Database</div>}
             {error && <div className='h-full grid place-content-center
                     text-2xl text-red-300'>{error.message}</div>}
-            {(!isLoading && !error) && <div id='table-container' className='min-w-0 h-screen overflow-auto'>
-                <table className='min-w-full'>
-                    <thead className='sticky top-0 bg-white text-left'>
-                        <tr>{columns.filter((c) => (c.selected)).map((col, index) =>
-                            <th colSpan={index === 0 ? 2 : 1} key={col.key}>
-                                <button
-                                    className='h-15 flex items-center gap-1 whitespace-nowrap w-full p-2 hover:bg-gray-300 border-b-1'
-                                    onClick={() => setSortBy((prev) => {
-                                        return (col.key === prev.key)
-                                            ? { ...prev, asc: !prev.asc }
-                                            : { key: col.key, asc: true }
-                                    })}
+            {(!isLoading && !error) && <div className='bg-white rounded-xl h-full pt-3'>
+                <div className='min-w-0 h-full overflow-auto px-3'>
+                    <div className='sticky px-2 flex items-center gap-1 justify-between'>
+                        <div className='flex items-center gap-1 text-gray-600'>
+                            <h1 className='text-3xl'>Legislators</h1> <span>({rows.length})</span>
+                        </div>
+                        <div className='flex gap-1'>
+                            <button className={`${buttonStyle} ${colSelectOpen ? 'bg-gray-200' : ''}`}
+                                onClick={() => setColSelectOpen(prev => !prev)}
+                            >
+                                <span className='material-symbols-outlined'
+                                    style={{ fontVariationSettings: `'FILL' ${colSelectOpen ? 1 : 0}` }}
                                 >
-                                    <span>{col.label}</span>
-                                    <span className={col.key !== sortBy.key ? 'invisible' : ''}>
-                                        {sortBy.asc ? '▲' : '▼'}</span>
-                                </button>
-                            </th>)
-                        }</tr>
-                    </thead>
-                    <tbody>
-                        {rows &&
-                            rows.map(row =>
-                                // (index >= pageSettings.index && index < pageSettings.index + pageSettings.rowsPerPage)
-                                <TableRow row={row} key={row.bioguide} />
-                            )}
-                    </tbody>
-                </table>
+                                    view_column
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                    <table className='min-w-full'>
+                        <thead className='sticky top-0 bg-white text-left'>
+                            <tr>{columns.filter((c) => (c.selected)).map((col, index) =>
+                                <th colSpan={index === 0 ? 2 : 1} key={col.key}>
+                                    <button
+                                        className='h-15 flex items-center gap-1 whitespace-nowrap w-full p-2
+                                            text-gray-500 hover:text-black hover:bg-gray-100 border-b-1 border-gray-300'
+                                        onClick={() => setSortBy((prev) => {
+                                            return (col.key === prev.key)
+                                                ? { ...prev, asc: !prev.asc }
+                                                : { key: col.key, asc: true }
+                                        })}
+                                    >
+                                        <span>{col.label}</span>
+                                        <span className={col.key !== sortBy.key ? 'invisible' : ''}>
+                                            {sortBy.asc ? '▲' : '▼'}</span>
+                                    </button>
+                                </th>)
+                            }</tr>
+                        </thead>
+                        <tbody>
+                            {rows &&
+                                rows.map(row =>
+                                    // (index >= pageSettings.index && index < pageSettings.index + pageSettings.rowsPerPage)
+                                    <TableRow row={row} key={row.bioguide} />
+                                )}
+                        </tbody>
+                    </table>
+                </div>
             </div>}
         </div>
     </>);
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import { useTableContext } from "../contexts/TableContext";
 import debounce from "../util/debounce";
+import FilterSelector from "./List/FilterSelector";
 
 export const buttonStyle = 'w-8 h-8 grid place-content-center rounded-full hover:bg-gray-200';
 
 export default function Search() {
     const { setFilterOptions, searchInput, setSearchInput } = useTableContext();
+    const [filterSelectOpen, setFilterSelectOpen] = useState(false);
+
     // const [waitingIndicator, setWaitingIndicator] = useState(false);
     const debouceTimeMs = 300;
     const debouncedSearch = debounce((value) => {
@@ -19,12 +22,13 @@ export default function Search() {
     }
 
     return (
-        <div className='flex-1 bg-white rounded-full'>
-            <label className='sr-only'>Search</label>
-            <div className='relative'>
-                <input className='w-full px-5 py-3 rounded-full
-                focus:outline-none focus:ring-2 focus:ring-gray-500
-                transition-color duration-300'
+        <div className={`relative flex-1 bg-gray-100 transition-[border-radius]
+        ${filterSelectOpen ? 'rounded-t-2xl delay-0' : 'rounded-2xl delay-300'}`}>
+            <div className='relative flex flex-col items-center'>
+                <label className='sr-only'>Search</label>
+                <input className={`z-20 w-full px-5 py-3 ${filterSelectOpen ? 'rounded-t-2xl' : 'rounded-2xl'}
+                    focus:outline-none focus:ring-2 focus:ring-gray-500
+                    transition-color duration-300`}
                     type='text'
                     placeholder='Search'
                     value={searchInput}
@@ -37,7 +41,26 @@ export default function Search() {
                         animate-spin [animation-duration:300ms]' />
                     </div>
                 )} */}
+                <button className={`absolute z-30 right-2 top-1/2 -translate-y-1/2
+                ${buttonStyle} ${true ? 'bg-gray-200' : ''}`}
+                    onClick={() => setFilterSelectOpen(prev => !prev)}>
+                    {/* onClick={() => { }}> */}
+                    <span className='material-symbols-outlined'
+                        style={{ fontVariationSettings: `'FILL' ${filterSelectOpen ? 1 : 0}` }}
+                    >
+                        filter_alt
+                    </span>
+                </button>
             </div>
-        </div>
+            <div className={`absolute z-10 bg-gray-100 rounded-b-2xl
+            grid ${filterSelectOpen ? 'grid-rows-[1fr] delay-100' : 'grid-rows-[0fr]'}
+            transition-[grid-template-rows] duration-300`}>
+                <div className='overflow-hidden'>
+                    <div className='py-2'>
+                        <FilterSelector />
+                    </div>
+                </div>
+            </div>
+        </div >
     );
 }
