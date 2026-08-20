4e22fa61c15dac92fd151ef1c0a4af769a974944
Michael Wong <40144792+glowmachine@users.noreply.github.com>
2026-08-20 20:03:51 +1000

    bug: fix ordering for falsy values

--- src/components/List/sortRows.ts
+++ src/components/List/sortRows.ts
@@ -5,12 +5,15 @@ export default function sortRows(rows: Row[], key: keyof RowData, asc: boolean):
         const aVal = a[key];
         const bVal = b[key];
 
-        if (aVal == null && bVal == null) return 0;
-        if (aVal == null) return asc ? 1 : -1;
-        if (bVal == null) return asc ? -1 : 1;
+        //both values empty
+        if (!aVal && !bVal) return 0;
 
+        //whichever value is empty goes to the bottom, ignore 0 (district)
+        if (aVal !== 0 && !aVal) return 1; //a empty, a goes after b
+        if (bVal !== 0 && !bVal) return -1; //b empty, a goes before b
+
+        //compare two non-empty values
         let compareVal = 0;
-
         if (typeof aVal === 'string' && typeof bVal === 'string')
             compareVal = aVal.localeCompare(bVal);
         else if (typeof aVal === 'number' && typeof bVal === 'number')
