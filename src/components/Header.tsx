5e5487a27d43701f7da2c3a16a75df3aa80ff127
Michael Wong <40144792+glowmachine@users.noreply.github.com>
2026-08-26 11:57:43 +1000

    feat: header title links to root

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,13 @@ export default function Header() {
     const isAbout: boolean = useLocation().pathname == '/about';
 
     return (
-        <header className='bg-red-200 px-3 py-2 flex justify-between items-center gap-3'>
-            <div className='flex items-center gap-3'>
-                <div className='text-5xl'>🇺🇸</div>
-                <h1 className='text-2xl'>USCL Viewer</h1>
-            </div>
+        <header className='bg-red-200 h-18 px-5 py-2 flex justify-between items-center gap-4'>
+            <NavLink to='/'>
+                <div className='flex items-center gap-2'>
+                    <div className='text-5xl'>🇺🇸</div>
+                    <h1 className='text-2xl'>USCL Viewer</h1>
+                </div>
+            </NavLink>
             {isRoot && <Search />}
             {!isAbout && <NavLink to='/about' className='size-10 rounded-full grid place-items-center hover:cursor-pointer bg-red-300'>?</NavLink>}
         </header>
