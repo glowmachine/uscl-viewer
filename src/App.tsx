15147b5435991a1d8916d36b485cd75195b55fbc
Michael Wong <40144792+glowmachine@users.noreply.github.com>
2026-08-26 16:08:35 +1000

    refactor: combine+delete redundant rules

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,11 @@ export default function App() {
       <TableProvider>
         <div className='h-screen flex flex-col'>
           <Header />
-          <main className='min-h-0 w-full bg-gray-300'>
-            <div className='p-6 flex flex-col min-w-0 max-h-full h-screen'>
-              <div className='relative bg-white rounded-3xl h-full pt-5'>
-                <DataProvider>
-                  <Outlet />
-                </DataProvider>
-              </div>
+          <main className='min-h-0 h-full p-6 flex flex-col bg-gray-300'>
+            <div className='relative h-full rounded-3xl pt-5 bg-white'>
+              <DataProvider>
+                <Outlet />
+              </DataProvider>
             </div>
           </main>
         </div>
